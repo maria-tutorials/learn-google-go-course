7e9f1ee0a05774a06d86e2b67277b12443c4064a
Maria Inês Serra <maria.serra@talkdesk.com>
2020-09-10 13:28:57 +0100

    lesson 46 - constants

--- 4_variables_values_types/main.go
+++ 4_variables_values_types/main.go
@@ -6,11 +6,19 @@ var x = 42
 
 var y float64
 
+const (
+	a float64 = 42.68
+	b bool    = true
+)
+
 func main() {
 	z := "Banana"
 	fmt.Printf("%v is of type  %T\n", x, x)
 	fmt.Printf("%v is of type %T\n", y, y)
 	fmt.Printf("%v is of type %T\n", z, z)
+
+	fmt.Printf("%v is of type %T\n", a, a)
+	fmt.Printf("%v is of type %T\n", b, b)
 
 	z = "Not banana"
 
