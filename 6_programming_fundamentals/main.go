77415f1fbc5b56cc8ccd036aeda15a49f4145079
Maria Inês Serra <maria.serra@talkdesk.com>
2020-09-10 13:39:04 +0100

    lesson 47 - bit shifting

--- 6_programming_fundamentals/main.go
+++ 6_programming_fundamentals/main.go
@@ -4,11 +4,14 @@ import (
 	"fmt"
 )
 
-func main() {
-	kb := 1024
-	mb := 1024 * kb
-	gb := 1024 * mb
+const (
+	_  = iota
+	kb = 1 << (iota * 10)
+	mb = 1 << (iota * 10)
+	gb = 1 << (iota * 10)
+)
 
+func main() {
 	fmt.Printf("%d\t\t\t%b\n", kb, kb)
 	fmt.Printf("%d\t\t\t%b\n", mb, mb)
 	fmt.Printf("%d\t\t%b\n", gb, gb)
