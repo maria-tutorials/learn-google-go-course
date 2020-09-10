1d1613fba04bfa9d5facdf6a440b14ceb3bf0398
Maria Inês Serra <maria.serra@talkdesk.com>
2020-09-10 14:24:02 +0100

    64. Conditional - switch statement

--- 8_control_flow/main.go
+++ 8_control_flow/main.go
@@ -5,6 +5,24 @@ import (
 )
 
 func main() {
+	switch {
+	case false:
+		fmt.Println("Won't print :(")
+	case (2 == 3):
+		fmt.Println("Won't print either :(")
+	case true:
+		fmt.Println("Will print :D")
+		fallthrough
+	case (2 == 2):
+		fmt.Println("Will also print")
+	case true:
+		fmt.Println("Will it print??")
+	}
+
+	moreSwitch()
+}
+
+func ourLoop() {
 	x := 1
 	for {
 		x++
@@ -20,4 +38,17 @@ func main() {
 
 	}
 	fmt.Println("done.")
+}
+
+func moreSwitch() {
+	fruit := "Cherry"
+
+	switch fruit {
+	case "Cherry", "Peach":
+		fmt.Println("Summer fruits")
+	case "Pear":
+		fmt.Println("is pear")
+	default:
+		fmt.Println("no bueno")
+	}
 }
