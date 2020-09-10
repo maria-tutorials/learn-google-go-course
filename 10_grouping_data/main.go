4d4d63f43212879f226644423de0a38434ec1685
Maria Inês Serra <maria.serra@talkdesk.com>
2020-09-10 16:05:44 +0100

    85. Map - introduction

--- 10_grouping_data/main.go
+++ 10_grouping_data/main.go
@@ -5,6 +5,11 @@ import (
 )
 
 func main() {
+	//slices()
+	maps()
+}
+
+func slices() {
 	x := []int{4, 5, 7, 8, 42}
 	fmt.Println(x)
 	x = append(x, 77, 88, 99, 1014)
@@ -17,4 +22,27 @@ func main() {
 	// Really cool way to remove data from slices at know positions
 	x = append(x[:2], x[4:]...)
 	fmt.Println(x)
+}
+
+func maps() {
+	//composite literal
+	m := map[string]int{
+		"Amália":  3,
+		"Eusébio": 2,
+	}
+	fmt.Println(m)
+
+	fmt.Println(m["Amália"])
+
+	fmt.Println(m["Lontra"])
+
+	// ,ok idiom !!!
+	v, ok := m["Lontra"]
+	fmt.Println(v)
+	fmt.Println(ok)
+
+	// IMPORTANT
+	if v, ok := m["Lontra"]; ok {
+		fmt.Println(v)
+	}
 }
