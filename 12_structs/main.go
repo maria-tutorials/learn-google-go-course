5bf29cf54e9e6efe39473c5d7a04f182fd45e539
Maria Inês Serra <14142563+mariaines00@users.noreply.github.com>
2020-09-23 15:47:36 +0100

    101. anonymous structs

--- 12_structs/main.go
+++ 12_structs/main.go
@@ -4,35 +4,17 @@ import (
 	"fmt"
 )
 
-type person struct {
-	first, last string
-	age         int
-}
-
-type secretAgent struct {
-	person
-	ltk bool
-}
-
 func main() {
-	sa1 := secretAgent{
-		person: person{
-			first: "James",
-			last:  "Bond",
-			age:   62,
-		},
-		ltk: true,
-	}
-
-	p2 := person{
-		first: "Also",
+	p1 := struct {
+		first, last string
+		age         int
+	}{
+		first: "James",
 		last:  "Bond",
-		age:   50,
+		age:   62,
 	}
 
-	fmt.Println(sa1)
-	fmt.Println(p2)
+	fmt.Println(p1)
 
-	fmt.Println(sa1.first, sa1.last, sa1.age, sa1.ltk)
-	fmt.Println(p2.first, p2.last, p2.age)
+	fmt.Println(p1.first, p1.last, p1.age)
 }
