2764957df47f5a4519d23d194d70840f5de24ad5
Maria Inês Serra <14142563+mariaines00@users.noreply.github.com>
2020-09-23 15:42:01 +0100

    99. embeded structs

--- 12_structs/main.go
+++ 12_structs/main.go
@@ -5,16 +5,23 @@ import (
 )
 
 type person struct {
-	first string
-	last  string
-	age   int
+	first, last string
+	age         int
+}
+
+type secretAgent struct {
+	person
+	ltk bool
 }
 
 func main() {
-	p1 := person{
-		first: "James",
-		last:  "Bond",
-		age:   32,
+	sa1 := secretAgent{
+		person: person{
+			first: "James",
+			last:  "Bond",
+			age:   62,
+		},
+		ltk: true,
 	}
 
 	p2 := person{
@@ -23,9 +30,9 @@ func main() {
 		age:   50,
 	}
 
-	fmt.Println(p1)
+	fmt.Println(sa1)
 	fmt.Println(p2)
 
-	fmt.Println(p1.first, p1.last, p1.age)
+	fmt.Println(sa1.first, sa1.last, sa1.age, sa1.ltk)
 	fmt.Println(p2.first, p2.last, p2.age)
 }
