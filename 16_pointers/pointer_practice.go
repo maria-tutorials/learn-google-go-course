42fead0941f56519eb0c15116bf83773771756d7
Maria Inês Serra <14142563+mariaines00@users.noreply.github.com>
2020-09-24 18:08:12 +0100

    step 2

--- 16_pointers/pointer_practice.go
+++ 16_pointers/pointer_practice.go
@@ -4,12 +4,17 @@ import "fmt"
 
 func main() {
 	x := 0
-	foo(x)
-	fmt.Println(x)
+	fmt.Println("before", x)
+	fmt.Println("before", &x)
+	foo(&x)
+	fmt.Println("after", x)
+	fmt.Println("after", &x)
 }
 
-func foo(x int) {
+func foo(x *int) {
 	fmt.Println(x)
-	x = 1970
+	fmt.Println(*x)
+	*x = 1970 //changing the underlying value in memory of the received address
 	fmt.Println(x)
+	fmt.Println(*x)
 }
