d10aea6cc3e65ca582e1c218685abc20407c3d7b
Maria Inês Serra <14142563+mariaines00@users.noreply.github.com>
2020-09-27 18:13:51 +0100

    152. Mutex 🤩🤩🤩

---
+++ 20_concurrency/mutex.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"fmt"
+	"runtime"
+	"sync"
+)
+
+var wg sync.WaitGroup
+var counter int
+
+var mu sync.Mutex
+
+func main() {
+	fmt.Println("# CPUs\t\t", runtime.NumCPU())
+	fmt.Println("Goroutines\t", runtime.NumGoroutine())
+
+	const gs = 100
+	wg.Add(gs)
+
+	for i := 0; i < gs; i++ {
+		go func() {
+			mu.Lock()
+			v := counter
+			runtime.Gosched()
+			v++
+			counter = v
+			mu.Unlock()
+			wg.Done()
+		}()
+		fmt.Println("Goroutines:", runtime.NumGoroutine())
+	}
+	wg.Wait()
+	fmt.Println("Goroutines\t", runtime.NumGoroutine())
+	fmt.Println("counter", counter)
+}
