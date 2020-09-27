fd72275d5416eb82d255a9ebf58255f516d28146
Maria Inês Serra <14142563+mariaines00@users.noreply.github.com>
2020-09-27 15:38:53 +0100

    139. sort

---
+++ 18_application/sort.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"fmt"
+	"sort"
+)
+
+func main() {
+	xi := []int{4, 7, 3, 42, 99, 18, 16, 56, 12}
+	xs := []string{"James", "Q", "M", "Moneypenny", "Dr. No"}
+
+	fmt.Println("before", xi)
+	sort.Ints(xi)
+	fmt.Println("after", xi)
+
+	if sort.IntsAreSorted(xi) {
+		fmt.Println("yey sorted ints")
+	}
+
+	fmt.Println("before", xs)
+	sort.Strings(xs)
+	fmt.Println("after", xs)
+
+	if sort.StringsAreSorted(xs) {
+		fmt.Println("yey sorted strings")
+	}
+}
