4bbc22a54b1b4210d8623f985f2cb336337acaba
Maria Inês Serra <14142563+mariaines00@users.noreply.github.com>
2020-09-27 16:38:37 +0100

    140. Sort custom (by name)

--- 18_application/custom_sort.go
+++ 18_application/custom_sort.go
@@ -16,6 +16,12 @@ func (a ByAge) Len() int           { return len(a) }
 func (a ByAge) Swap(i, j int)      { a[i], a[j] = a[j], a[i] }
 func (a ByAge) Less(i, j int) bool { return a[i].age < a[j].age }
 
+type ByName []Person
+
+func (bn ByName) Len() int           { return len(bn) }
+func (bn ByName) Swap(i, j int)      { bn[i], bn[j] = bn[j], bn[i] }
+func (bn ByName) Less(i, j int) bool { return bn[i].name < bn[j].name }
+
 func main() {
 	p1 := Person{"Captain", 72}
 	p2 := Person{"Natasha", 32}
@@ -27,6 +33,14 @@ func main() {
 	fmt.Println("before", people)
 
 	sort.Sort(ByAge(people))
+
+	fmt.Println("after", people)
+
+	fmt.Println("-------")
+
+	fmt.Println("before", people)
+
+	sort.Sort(ByName(people))
 
 	fmt.Println("after", people)
 }
