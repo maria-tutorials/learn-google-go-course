2be32d4548e10580c8b8cb9c67fc9741d38f37fd
Maria Inês Serra <14142563+mariaines00@users.noreply.github.com>
2020-09-27 19:25:52 +0100

    181. Errors with info (now using a struct)

--- 24_error_handling/custom_error.go
+++ 24_error_handling/custom_error.go
@@ -5,6 +5,16 @@ import (
 	"log"
 )
 
+type myError struct {
+	lat  string
+	long string
+	err  error
+}
+
+func (m myError) Error() string {
+	return fmt.Sprintf("an error occured in: %v %v %v", m.lat, m.long, m.err)
+}
+
 func main() {
 	_, err := sqrt(-10)
 	if err != nil {
@@ -14,7 +24,8 @@ func main() {
 
 func sqrt(n float64) (float64, error) {
 	if n < 0 {
-		return 0, fmt.Errorf("Negative number: %v", n)
+		merr := fmt.Errorf("Negative number: %v", n)
+		return 0, myError{"50.99N", "99.50W", merr}
 		//errors.New("Negative number 👎 ")
 	}
 	return 42, nil
